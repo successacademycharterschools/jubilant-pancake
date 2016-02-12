ef45226555397ea6421e0845908a3adbaa65736f
Paul Curry <pcurry@gmail.com>
2016-02-12 11:56:23 -0500

    Get some automated testing running

---
+++ src/python/test_webapp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+import unittest
+
+from webob import Request
+from webob import Response
+
+from webapp import StringComparitorApp
+
+
+class TestStringComparitor(unittest.TestCase):
+
+    def test_get(self):
+        app = StringComparitorApp()
+        req = Request.blank('')
+        response = app(req)
+
+
+    def test_post(self):
+        app = StringComparitorApp()
+        req = Request.blank('/compare')
+        req.method = 'POST'
+        response = app(req)
+
+    def test_put(self):
+        app = StringComparitorApp()
+        req = Request.blank('')
+        req.method = 'PUT'
+        response = app(req)
+        self.assertEqual(500, response.status_code)
+
+
+if __name__ == "__main__":
+    unittest.main()
