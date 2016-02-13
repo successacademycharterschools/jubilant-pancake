65ab78723490c3d2523dbf61e34cef0319fa946a
Paul Curry <pcurry@gmail.com>
2016-02-13 01:02:41 -0500

    Don't hardcode setup parameters, use config

--- src/python/test_webapp.py
+++ src/python/test_webapp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 
+import ConfigParser
 import unittest
 
 from webob import Request
@@ -10,19 +11,30 @@ from webapp import StringComparitorApp
 
 class TestStringComparitor(unittest.TestCase):
 
+    def setUp(self):
+        super(TestStringComparitor, self).setUp()
+        self.app_config = ConfigParser.SafeConfigParser()
+        with open("defaults.cfg", "r") as config_defaults:
+            self.app_config.readfp(config_defaults)
+
+    def tearDown(self):
+        del self.app_config
+        super(TestStringComparitor, self).tearDown()
+
     def test_get(self):
-        app = StringComparitorApp()
+        app = StringComparitorApp(self.app_config)
         req = Request.blank('')
         response = app(req)
+        self.assertNotEqual('', response.body)
 
     def test_post(self):
-        app = StringComparitorApp()
+        app = StringComparitorApp(self.app_config)
         req = Request.blank('/compare')
         req.method = 'POST'
         response = app(req)
 
     def test_put(self):
-        app = StringComparitorApp()
+        app = StringComparitorApp(self.app_config)
         req = Request.blank('')
         req.method = 'PUT'
         response = app(req)
--- src/python/webapp.py
+++ src/python/webapp.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python2.7
 
+import ConfigParser
+
+
 from webob import Request
 from webob import Response
 from webob import dec
-from webob import exc
 
 
 class StringComparitorApp(object):
 
-    def __init__(self):
+    def __init__(self, config):
         self.dispatch = {
             "GET": self.page,
             "POST": self.compare,
         }
+        with open(config.get("web", "page_filename"), "r") as pf:
+            self.page_content = pf.read()
 
     @dec.wsgify
     def __call__(self, req):
@@ -31,18 +35,30 @@ class StringComparitorApp(object):
     @dec.wsgify
     def page(self, req):
         response = Response()
+        response.body = self.page_content
         return response
 
 
-def main():
-    app = StringComparitorApp()
+def run_server(app, config):
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', '5678', app)
+    host = config.get("site", "host")
+    port = config.get("site", "port")
+    httpd = make_server(host, port, app)
     print 'Serving on http://localhost:5678'
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print '^C'
+
+
+def main():
+    config = ConfigParser.SafeConfigParser()
+    with open("defaults.cfg", "r") as config_defaults:
+        config.readfp(config_defaults)
+
+    app = StringComparitorApp(config)
+
+    run_server(app, config)
 
 
 if __name__ == "__main__":
