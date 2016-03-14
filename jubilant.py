fa8758fd16c3d22fc69f672ecb17c645711c06fc
andrew martin <agmartin>
2016-03-14 08:07:12 -0400

    handles levenshtein edge cases to always return dict. adds html forms.

--- jubilant.py
+++ jubilant.py
@@ -32,9 +32,7 @@ def levenshtein(string1=None, string2=None):
         for j in range(len(v0)):
             v0[j] = v1[j]
 
-    dist = {"dist": v1[len(string2)]}
-
-    return dist
+    return v1[len(string2)]
 
 
 class MyRequestHandler(BaseHTTPRequestHandler):
@@ -54,10 +52,10 @@ class MyRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-        # print(self.data_string)
+        print(self.data_string)
 
         levenshtein_args = json.loads(self.data_string.decode("utf-8"))
-        # print(levenshtein_args)
+        print(levenshtein_args)
 
         self.send_response(200)
         self.send_header("Content-type:", "text/html")
@@ -65,8 +63,8 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         # self.end_headers()  ##Seriously. Find out why this doesn't work.
         self.wfile.write(bytes("\n", 'utf-8'))
 
-        dist = levenshtein(levenshtein_args['string1'],
-                           levenshtein_args['string2'])
+        dist = {"dist": levenshtein(levenshtein_args['string1'],
+                           levenshtein_args['string2'])}
         print(dist)
         data = json.dumps(dist)
         self.wfile.write(bytes(data, 'utf-8'))
