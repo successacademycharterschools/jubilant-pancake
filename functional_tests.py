f8b55752d946c37eee954ae3c071bf31958a61a8
Edinunzio <e.dinunzio@gmail.com>
2016-04-08 03:15:11 -0400

    Stubbing functional tests
     - introduced intentionally failing test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_submit_form_returns_dist(self):
+        # User goes to Jubilant Pancake homepage
+        self.browser.get('http://localhost:8000')
+
+        # User sees page title
+        self.assertIn('Jubilant Pancake', self.browser.title)
+        self.fail('Finish test! Pancake not fully jubilant operational')
+
+        # User sees a paragraph about the site
+        # User sees 2 text input fields and a centered placeholder
+        # User types into first input
+        # User types into second input
+        # User submits form
+        # User sees placeholder contains minimum edit distance
+
+        # User types into first input
+        # User types into second input
+        # User submits form
+        # User sees placeholder contains minimum edit distance
+
+        # User quits browser
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
