b6c734a962b0a12d231920d865f41af3e94bbfcc
Edinunzio <e.dinunzio@gmail.com>
2016-04-10 00:44:49 -0400

    UI tweaks and best practices
     - removed javascript from html

--- jubilantpancake/editdistances/static/js/main.js
+++ jubilantpancake/editdistances/static/js/main.js
@@ -1,5 +1,22 @@
 $(function() {
+    $('#input_1').val('');
+    $('#input_2').val('');
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
     var submit_btn = $('.submit');
+    var csrftoken = getCookie('csrftoken');
     $('.submit').on('click', function(){
         var input_1 = $.trim($('#input_1').val());
         var input_2 = $.trim($('#input_2').val());
