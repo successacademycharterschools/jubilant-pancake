c299fb29ffdeb2a5ff2ba2b33a9fdb467425e29d
Thomas Robertson <thomas.e.robertson@gmail.com>
2016-06-07 17:11:09 -0400

    Add some styles to make things look less terrible.

--- static/js/minimum-edit-distance/app.js
+++ static/js/minimum-edit-distance/app.js
@@ -27,6 +27,7 @@ angular.module('MinimumEditDistance', [])
 
                 ctrl.submit_form = function() {
                     medService.calculate(ctrl.str1, ctrl.str2).then(function onSuccess(response) {
+                        ctrl.show_results = true;
                         ctrl.show_str1 = ctrl.str1;
                         ctrl.show_str2 = ctrl.str2;
                         ctrl.step_count = response.data.steps;
