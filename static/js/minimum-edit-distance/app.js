c70c43a620b407e8bc1cae717f1ca9ccd5637358
Thomas Robertson <thomas.e.robertson@gmail.com>
2016-06-07 16:45:23 -0400

    Create an Angular service to call our MED API and wire it up.

--- static/js/minimum-edit-distance/app.js
+++ static/js/minimum-edit-distance/app.js
@@ -1,17 +1,36 @@
 angular.module('MinimumEditDistance', [])
+    .factory('medService', ['$http', function($http) {
+        return {
+            calculate: function(str1, str2) {
+                return $http({
+                    method: 'POST',
+                    url: '/api/edit-distance/',
+                    data: JSON.stringify({
+                        str1: str1,
+                        str2: str2
+                    })
+                })
+            }
+        }
+    }])
     .directive('medForm', function() {
         return {
             restrict: 'E',
             templateUrl: '/static/js/minimum-edit-distance/partials/med-form.html',
-            controller: ['$scope', function($scope) {
-                // Set our initial values
-                this.str1 = '';
-                this.str2 = '';
+            controller: ['medService', function(medService) {
+                // Alias our this value so we can use it in nested scopes
+                ctrl = this;
 
-                this.submit_form = function() {
-                    this.show_str1 = this.str1;
-                    this.show_str2 = this.str2;
-                    this.step_count = 10;
+                // Set our initial values
+                ctrl.str1 = '';
+                ctrl.str2 = '';
+
+                ctrl.submit_form = function() {
+                    medService.calculate(ctrl.str1, ctrl.str2).then(function onSuccess(response) {
+                        ctrl.show_str1 = ctrl.str1;
+                        ctrl.show_str2 = ctrl.str2;
+                        ctrl.step_count = response.data.steps;
+                    });
                 };
             }],
             controllerAs: 'medCtrl'
