65c25a5b26167378d522feaa2ab3753b0a7d0a06
Thomas Robertson <thomas.e.robertson@gmail.com>
2016-06-07 16:26:53 -0400

    Clean up the interactions and text for the MED frontend.

--- static/js/minimum-edit-distance/app.js
+++ static/js/minimum-edit-distance/app.js
@@ -1,13 +1,18 @@
 angular.module('MinimumEditDistance', [])
-    .controller('MinimumEditDistanceController', function() {
-        console.log('loaded controller');
-    })
     .directive('medForm', function() {
         return {
             restrict: 'E',
             templateUrl: '/static/js/minimum-edit-distance/partials/med-form.html',
             controller: ['$scope', function($scope) {
-                this.click_me = function() { alert('clicked me!'); };
+                // Set our initial values
+                this.str1 = '';
+                this.str2 = '';
+
+                this.submit_form = function() {
+                    this.show_str1 = this.str1;
+                    this.show_str2 = this.str2;
+                    this.step_count = 10;
+                };
             }],
             controllerAs: 'medCtrl'
         };
