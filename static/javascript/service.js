ee77b5960b776d4929c518c9af0c255ca14c1933
Chris <cbaca01@gmail.com>
2018-07-24 14:22:31 -0400

    Made style changes to the webform:
    - static/css/styles.css: added the main styles to this style-sheet for the webform
    - static/javascript/service.js: made result message more user-friendly
    - templates/appForm.html: put message output inside web-form.

--- static/javascript/service.js
+++ static/javascript/service.js
@@ -12,9 +12,16 @@ function formController($scope, $http) {
 
 	// process the form
 	$scope.submitForm = function() {
+
+		// if no variables are entered or the formData is empty, then we do not make a service call
+		if (Object.keys($scope.formData).length == 0 || ($scope.formData.str1 == '' && $scope.formData.str2 == '')) {
+			$scope.result = 'You have to enter two strings to calculate the edit distance';
+			return false;
+		}
+
 		$http.post('http://127.0.0.1:5002/test_string', $scope.formData)
 			.success(function(data) {
-				$scope.result = data.moves;
+				$scope.result = data.moves + ' moves would be needed';
 			}
 		);
 	};
