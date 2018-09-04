6652585f5cd5e2a991c6eabdd1d07e9897b69d2e
Marley Alford <marleycalford@gmail.com>
2018-09-04 00:31:29 -0400

    Added EditDistanceForm component.  Accepts two inputs and prints to console on submit.

--- jubilant-pancake/src/App.js
+++ jubilant-pancake/src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class EditDistanceForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValues: {'val_1': '', 'val_2': ''}
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    let formValues = this.state.formValues;
+    let name = event.target.name;
+    let value = event.target.value;
+
+    formValues[name] = value;
+
+    this.setState({formValues})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.formValues);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <p>
+            Input 1:
+            <input type="text" name="val_1" value={this.state.formValues['val_1']} onChange={this.handleChange} />
+          </p>
+          <p>
+            Input 2:
+            <input type="text" name="val_2" value={this.state.formValues['val_2']} onChange={this.handleChange} />
+          </p>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Calculate Edit Distance</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NameForm />
       </div>
     );
   }
