0766ab6ee797d71e4d8192c3a3a622fedc0c1c3c
Marley Alford <marleycalford@gmail.com>
2018-09-04 23:13:00 -0400

    StringInputForm successfully sends values back up to parent component.

--- jubilant-pancake/src/App.js
+++ jubilant-pancake/src/App.js
@@ -4,15 +4,49 @@ import './App.css';
 class StringInputForm extends Component {
   constructor(props) {
     super(props);
+    this.handleValueInputChange = this.handleValueInputChange.bind(this);
+    this.handleValueInputSubmit = this.handleValueInputSubmit.bind(this);
+  }
+  
+  handleValueInputChange(e) {
+    this.props.onValueChange(e);
+  }
+
+  handleValueInputSubmit(e) {
+    this.props.onValueSubmit(e);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleValueInputSubmit}>
+        <label>
+          <p>
+            Input 1:
+            <input type="text" name="val_1" value={this.props.formValues['val_1']} onChange={this.handleValueInputChange} />
+          </p>
+          <p>
+            Input 2:
+            <input type="text" name="val_2" value={this.props.formValues['val_2']} onChange={this.handleValueInputChange} />
+          </p>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       formValues: {'val_1': '', 'val_2': ''}
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    
+    this.handleValueChange = this.handleValueChange.bind(this); // for sending input back up the hierarchy
+    this.handleValueSubmit = this.handleValueSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleValueChange(event) {
     event.preventDefault();
     let formValues = this.state.formValues;
     let name = event.target.name;
@@ -23,38 +57,22 @@ class StringInputForm extends Component {
     this.setState({formValues})
   }
 
-  handleSubmit(event) {
+  handleValueSubmit(event) {
     event.preventDefault();
     console.log(this.state.formValues);
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <p>
-            Input 1:
-            <input type="text" name="val_1" value={this.state.formValues['val_1']} onChange={this.handleChange} />
-          </p>
-          <p>
-            Input 2:
-            <input type="text" name="val_2" value={this.state.formValues['val_2']} onChange={this.handleChange} />
-          </p>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Calculate Edit Distance</h1>
         </header>
-        <StringInputForm />
+        <StringInputForm
+          formValues={this.state.formValues}
+          onValueChange={this.handleValueChange}
+          onValueSubmit={this.handleValueSubmit}
+        />
       </div>
     );
   }
