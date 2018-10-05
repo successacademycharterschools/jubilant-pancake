42b30c30389aeb51aa7b6c770d744132bc66cebe
Zachary Spector <public@zacharyspector.com>
2018-10-05 17:18:14 -0400

    Add an end-to-end test
    Requires pytest-flask.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,22 @@
 import pytest
-from flask import url_for
-from ..main import create_app
+from .data import edit_distance_test_data
+from main import create_app
 
 
 @pytest.fixture
 def app():
     app = create_app()
     return app
+
+
+@pytest.mark.parametrize(
+    ['source', 'target', 'distance'],
+    edit_distance_test_data
+)
+def test_app(client, source, target, distance):
+    resp = client.get('/{}/{}'.format(source, target))
+    assert resp.status_code == 200
+    assert int(resp.data) == distance, \
+        "For strings {}/{}, app returned {} (should be {})".format(
+            source, target, resp.data, distance
+        )
