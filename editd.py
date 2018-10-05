166d15d53d00126f93af195733590bedad693d60
Zachary Spector <public@zacharyspector.com>
2018-10-05 12:01:36 -0400

    Implement Levenshtein edit distance function

---
+++ editd.py
@@ -0,0 +1,32 @@
+"""Functions for computing edit distance
+
+Currently this exports only the ``edit_distance`` function, which is a
+hinted implementation of the Levenshtein algorithm.
+
+"""
+
+
+def _edit_distance_recursive(a, b, i, j):
+    if min((i, j)) == 0:
+        ret = _edit_distance_recursive.hint[a, b, i, j] = max((i, j))
+        return ret
+    if (a, b, i, j) not in _edit_distance_recursive.hint:
+        _edit_distance_recursive.hint[a, b, i, j] = min((
+            _edit_distance_recursive(a, b, i - 1, j) + 1,
+            _edit_distance_recursive(a, b, i, j - 1) + 1,
+            _edit_distance_recursive(a, b, i - 1, j - 1) + (
+                0 if a[i - 1] == b[j - 1] else 1
+            )
+        ))
+    return _edit_distance_recursive.hint[a, b, i, j]
+
+
+_edit_distance_recursive.hint = {}
+
+
+def edit_distance(a, b):
+    """Hinted Levenshtein algorithm for edit distance"""
+    return _edit_distance_recursive(a, b, len(a), len(b))
+
+
+__all__ = ['edit_distance']
