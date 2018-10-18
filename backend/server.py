e3136145c58657088bed120c7cecb9ec59d28637
Raptor Dz <gregdzuricsko@gmail.com>
2018-10-18 11:49:10 -0400

    pylint

--- backend/server.py
+++ backend/server.py
@@ -1,43 +1,51 @@
-from flask import Flask, jsonify, request
-import unittest
-app = Flask(__name__)
+"""This flask web server takes two arguments, val1 and val2 as strings.
+Then it computes the distance between the two character by character"""
 
-@app.route("/")
+import unittest
+from flask import Flask, jsonify, request
+APP = Flask(__name__)
+
+@APP.route("/")
 def hello():
-    val1 = request.args.get('val1')
-    val2 = request.args.get('val2')
-    result = computeDistance(val1, val2)
+    """For this simple code test, we just need one endpoint for the front end to hit"""
+    val_one = request.args.get('val1')
+    val_two = request.args.get('val2')
+    result = compute_distance(val_one, val_two)
     response = jsonify({'result':result})
-    # for a code test, just let cors through. 
+    # for a code test, just let cors through.
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-def computeDistance(val1, val2):
+def compute_distance(val_one, val_two):
+    """Compute the distance, char by char, between the two strings.
+    This assumes that they are not null."""
     #to be returned
-    opCount = 0
+    op_count = 0
 
     #turn strings into lists of characters to be worked with
-    val1List = list(val1)
-    val2List = list(val2)
+    val_one_list = list(val_one)
+    val_two_list = list(val_two)
 
-    #the for loop below is a bit easier when you know which is bigger
-    biggerList = val2List
-    smallerList = val1List
-    if len(val1List) > len(val2List):
-        biggerList = val1List
-        smallerList = val2List
+    #the loop below is a bit easier when you know which is bigger
+    bigger_list = val_two_list
+    smaller_list = val_one_list
+    if len(val_one_list) > len(val_two_list):
+        bigger_list = val_one_list
+        smaller_list = val_two_list
 
-    for i in range(len(smallerList)):
-        if i < len(biggerList):
-            if smallerList[i] != biggerList[i]:
-                opCount += 1
-    opCount += (len(biggerList) - len(smallerList))
-    return opCount
+    for i, small_list_item in enumerate(smaller_list):
+        if i < len(bigger_list):
+            if small_list_item != bigger_list[i]:
+                op_count += 1
+    op_count += (len(bigger_list) - len(smaller_list))
+    return op_count
 
 
-class TestStringMethod(unittest.TestCase):
-    def test_test(self):
-        self.assertEqual(computeDistance("test","test"), 0)
-        self.assertEqual(computeDistance("",""), 0)
-        self.assertEqual(computeDistance("12345","123456"), 1)
-        self.assertEqual(computeDistance("123456","12345"), 1)
+class TestComputeDistance(unittest.TestCase):
+    """ a simple class for testing the compute distance method used by the server"""
+    def test_compute_distance(self):
+        """ a simple couple test cases for the compute distance method."""
+        self.assertEqual(compute_distance("test", "test"), 0)
+        self.assertEqual(compute_distance("", ""), 0)
+        self.assertEqual(compute_distance("12345", "123456"), 1)
+        self.assertEqual(compute_distance("123456", "12345"), 1)
